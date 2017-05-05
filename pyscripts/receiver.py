a02908c5f12e2e1a1d4cfd47bcb9a352ed0ae43a
Jack Lorey <jlorey@gmail.com>
2017-05-05 18:09:14 -0500

    minor stuff

--- pyscripts/receiver.py
+++ pyscripts/receiver.py
@@ -12,7 +12,7 @@ import logging; logging.basicConfig(level=logging.DEBUG)
 
 
 class receiver(threading.Thread):
-    
+
     def __init__(self, addr, user):
         threading.Thread.__init__(self)
         self.addr = addr
@@ -22,11 +22,11 @@ class receiver(threading.Thread):
           on_message = self.on_message,
           on_error = self.on_error,
           on_close = self.on_close)
-    
+
         self.ws.on_open = self.on_open
         self.pos = pd.DataFrame(columns=['balance', 'btcs', 'last'])
-        
-        
+
+
     def run(self):
         self.ws.run_forever()
         return
@@ -35,7 +35,7 @@ class receiver(threading.Thread):
         self.msg = json.loads(message)
         fee = 1 - 0.00
         for coin, val in self.msg['balances'].iteritems():
-            try: 
+            try:
                 balance = (float(val['onOrders']) + float(val['available']))
                 if coin == 'USDT':
                     last = (float(self.msg['marketData']['USDT_BTC']['lowestAsk']) + float(self.msg['marketData']['USDT_BTC']['highestBid'])) / 2
@@ -45,35 +45,18 @@ class receiver(threading.Thread):
                 else:
                     last = (float(self.msg['marketData']['BTC_' + coin]['lowestAsk']) + float(self.msg['marketData']['BTC_' + coin]['highestBid'])) / 2
                     self.pos.ix[coin] = [balance, (balance * last) * fee, last]
-                          
+
             except:
                 pass
-            
+
         self.total = self.pos['btcs'].sum() * self.pos['last']['USDT']
-                
+
     def on_error(self, ws, error):
         print error
-    
+
     def on_close(self, ws):
         print "### closed ###"
-    
+
     def on_open(self, ws):
         self.ws.send(self.user)
         print "connection opened"
-
-
-addr = "ws://205.178.62.72:8888/"
-addr = "ws://localhost:8888/"
-r = receiver(addr, 'jack')
-r.start()
-
-
-
-
-
-
-
-
-
-
-
